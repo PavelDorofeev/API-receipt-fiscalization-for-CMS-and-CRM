163222911c22d3d96b87d04f03c4edd67e137709
Pavel <p@kkmspb.ru>
2025-06-17 19:48:04 +0300

    поправли пробитие чеков на большое количество позиций (80 и т.д.)

--- test-api-page/test-from-php.php
+++ test-api-page/test-from-php.php
@@ -36,9 +36,15 @@ $unic_id = mt_rand();
 			<button name="action" value="kktCashIn"     type="submit">Внесение</button> 
 			<button name="action" value="kktCashOut"    type="submit">Изъятие</button> </br>
 			<button name="action" value="kktReceiptFiscalization" type="submit">Чек</button> 
-			<button name="action" value="kktReceiptFiscalization_big" type="submit">Чек (20 позиций)</button> 
+			<button name="action" value="kktReceiptFiscalization_20" type="submit">Чек (20 позиций)</button> 
+			<button name="action" value="kktReceiptFiscalization_40" type="submit">Чек (40 позиций)</button> 
+			<button name="action" value="kktReceiptFiscalization_80" type="submit">Чек (80 позиций)</button> 
 			<button name="action" value="kktXReport"    type="submit">X отчет</button> </br>
 			<button name="action" value="kktOpenCashDraw"    type="submit">открыть денежный ящик</button> </br>
+			<label>токен ккт:
+				<input name="BIT_KKT_TOKEN" value="<?php echo $_GET['BIT_KKT_TOKEN'];?>"    type="text" size="32"/> 
+			</label></br>
+			
 			<input name="cnt" type="hidden" value="<?php echo $unic_id;?>"/>
 		</form>
 	</div>
@@ -193,9 +199,9 @@ else if( $_GET["action"]=="kktCashOut")
 else if( $_GET["action"]=="kktReceiptFiscalization")
 	$payload = json_encode( $BIT_RECEIPT );
 
-else if( $_GET["action"]=="kktReceiptFiscalization_big")
+else if( $_GET["action"]=="kktReceiptFiscalization_20")
 {
-	$summ = addMorePurchases($BIT_RECEIPT[0]["data"]);
+	$summ = addMorePurchases($BIT_RECEIPT[0]["data"] , 20 , "наименование тестового товара 1235");
 	//echo "$summ=".$summ;
 	//echo "BIT_RECEIPT[payments][cash_1031]".$BIT_RECEIPT[0]["data"]["payments"]["cash_1031"];
 	
@@ -203,6 +209,22 @@ else if( $_GET["action"]=="kktReceiptFiscalization_big")
 	
 	$payload = json_encode( $BIT_RECEIPT );
 	//echo "<BR>payload: <pre>" .json_encode($BIT_RECEIPT , JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT)."</pre><BR>\n";
+}
+else if( $_GET["action"]=="kktReceiptFiscalization_40")
+{
+	$summ = addMorePurchases($BIT_RECEIPT[0]["data"] , 40 , "Отладка Программы №1 Тест на длинные названия и большое количество артикулов. Отладка программы много позиций.");
+	
+	$BIT_RECEIPT[0]["data"]["payments"]["cash_1031"] = $BIT_RECEIPT[0]["data"]["payments"]["cash_1031"] + $summ ;
+	
+	$payload = json_encode( $BIT_RECEIPT );
+}
+else if( $_GET["action"]=="kktReceiptFiscalization_80")
+{
+	$summ = addMorePurchases($BIT_RECEIPT[0]["data"] , 80 , "Отладка Программы №1 Тест на длинные названия и большое количество артикулов. Отладка программы много позиций.");
+	
+	$BIT_RECEIPT[0]["data"]["payments"]["cash_1031"] = $BIT_RECEIPT[0]["data"]["payments"]["cash_1031"] + $summ ;
+	
+	$payload = json_encode( $BIT_RECEIPT );
 }
 
 else if( $_GET["action"]=="kktXReport")
@@ -223,19 +245,25 @@ curl_setopt( $ch, CURLOPT_POST, 1);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 
 			//'Content-Length: '.mb_strlen($payload),
-curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+$BIT_KKT_TOKEN='';
+if( array_key_exists('BIT_KKT_TOKEN' , $_GET ) )
+	$BIT_KKT_TOKEN= $_GET['BIT_KKT_TOKEN'];
+	
+		//'BIT_KKT_TOKEN: 16bba36069e6a91d4919fa64c9236f4e', //мерк
+		//'BIT_KKT_TOKEN: 435cb88c28fc49bd419d58d4b60680b5', // атол 1.05 435cb88c28fc49bd419d58d4b60680b5  
+		// ddf3cfc947c5fe3ee7ad96660c269e07', // атол 1.2
+		// штрих 12d1ed895b8b5bc60137d68492e88017
+		// атол 1.2 ddf3cfc947c5fe3ee7ad96660c269e07
+$headers = array(
 			'Content-Type: application/json',
 			'Action: command_list',
 			'BIT_ENCODE_TYPE: PHP', 
 			'BIT_ORDER_ID: 122',
-			//'BIT_KKT_TOKEN: 16bba36069e6a91d4919fa64c9236f4e', //мерк
-			//'BIT_KKT_TOKEN: 435cb88c28fc49bd419d58d4b60680b5', // атол 1.05 435cb88c28fc49bd419d58d4b60680b5  
-			'BIT_KKT_TOKEN: ddf3cfc947c5fe3ee7ad96660c269e07', // атол 1.2
-			// штрих 12d1ed895b8b5bc60137d68492e88017
-			// атол 1.2 ddf3cfc947c5fe3ee7ad96660c269e07
-			//'BIT_BNK_TRM_TOKEN: 7a6635341f95c54f4f6556c5940ed459',
+			'BIT_KKT_TOKEN: '.$BIT_KKT_TOKEN.'', 
 			'Origin: https://kkmspb.ru'
-        ) );
+        ) ;		
+		
+curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
 curl_close($ch);
@@ -246,7 +274,10 @@ echo "
 
 
 echo "
-	<h2>\n послали:</h2>
+	<h2>Послали:</h2>
+	<h3>заголовки:</h2>
+	<pre>" . json_encode(  $headers  , JSON_PRETTY_PRINT)."</pre>
+	<h3>тело:</h2>
 	<pre>" . json_encode( json_decode ( $payload , true ) , JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)."</pre>";
 ?>
 
@@ -255,11 +286,27 @@ echo "
 
 <?php
 
-function addMorePurchases(&$data)
+function addMorePurchases(&$data, $count, $productName)
 {
+	$item = array(
+			'productName_1030'=>"$productName",
+			'price_1079'=>0,
+			'qty_1023'=>1,
+			"amount_1043"=>0,
+			'unit_2108'=>0,
+			'paymentFormCode_1214'=>4,
+			'productTypeCode_1212'=>1,
+			'tax_1199'=>6
+		);
 	
-$big= array(
-		array(
+	$big= array();
+
+	for( $ii=0; $ii<$count; $ii++)
+	{
+		$big[] = $item;
+	}
+	
+/*		array(
 			'productName_1030'=>'Отладка программы',
 			'price_1079'=>0,
 			'qty_1023'=>1,
@@ -459,8 +506,8 @@ $big= array(
 			'productTypeCode_1212'=>1,
 			'tax_1199'=>6
 		)
-	);
-		
+	);*/
+	
 	$summ=0;
 	foreach( $big as  $vv)
 	{
