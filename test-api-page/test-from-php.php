8b9f40fe3132ea4ef846616e87a32751da410ab1
Pavel <p@kkmspb.ru>
2025-08-07 17:29:04 +0300

    добавляем маркировку разрешительный режим для атола

--- test-api-page/test-from-php.php
+++ test-api-page/test-from-php.php
@@ -27,11 +27,11 @@ $unic_id = mt_rand();
 
 ?>
 
-<body>
+<body style="width:100%">
 
-	<div>
-	<p>Вы можете посмотреть содержание этой php страницы здесь: <a href="https://github.com/PavelDorofeev/API-receipt-fiscalization-for-CMS-and-CRM/tree/master/test-api-page">на github.</a></p>
+	<h2>Вы можете посмотреть содержание этой php страницы здесь: <a href="https://github.com/PavelDorofeev/API-receipt-fiscalization-for-CMS-and-CRM/tree/master/test-api-page">на github.</a></h2>
 	
+	<div>
 	
 	
 		<form method="GET">
@@ -40,10 +40,10 @@ $unic_id = mt_rand();
 			<button name="action" value="kktCashIn"     type="submit">Внесение</button> 
 			<button name="action" value="kktCashOut"    type="submit">Изъятие</button> </br>
 			<button name="action" value="kktReceiptFiscalization" type="submit">Чек</button> 
-			<button name="action" value="kktReceiptFiscalization_M" type="submit">Чек с маркировкой</button> 
-			<button name="action" value="kktReceiptFiscalization_20" type="submit">Чек (20 позиций)</button> 
-			<button name="action" value="kktReceiptFiscalization_40" type="submit">Чек (40 позиций)</button> 
-			<button name="action" value="kktReceiptFiscalization_80" type="submit">Чек (80 позиций)</button> 
+			<button name="action" value="kktReceiptFiscalization_M" type="submit">Чек с маркировкой ФФД 1.2 <br>разрешительный режим</button> 
+			<button name="action" value="kktReceiptFiscalization_20" type="submit">Чек<br>(20 позиций)</button> 
+			<button name="action" value="kktReceiptFiscalization_40" type="submit">Чек<br>(40 позиций)</button> 
+			<button name="action" value="kktReceiptFiscalization_80" type="submit">Чек<br>(80 позиций)</button> 
 			<button name="action" value="kktXReport"    type="submit">X отчет</button> </br>
 			<button name="action" value="kktOpenCashDraw"    type="submit">открыть денежный ящик</button> </br>
 			<label title="из лк на kkmspb.ru">токен ккт:
@@ -53,7 +53,7 @@ $unic_id = mt_rand();
 			<input name="cnt" type="hidden" value="<?php echo $unic_id;?>"/>
 
 			<label title="проверка КМ через ККТ/ФН/ОФД ИСМ">считайте код маркировки:
-				<input name="KM" value="<?php echo $_GET['KM'];?>" type="text" size="150"/> 
+				<input name="KM" value="<?php echo $_GET['KM'];?>" type="text" size="150" placeholder="пока не работает"/> 
 			</label>		
 			<button name="action" value="kktCheckKM" type="submit">Отправить</button>
 			</br>
@@ -122,30 +122,21 @@ $BIT_RECEIPT_WITH_MARKING = [
 				'productTypeCode_1212'=>1,
 				'tax_1199'=>6,
 
-				'imcType_2100' => 3, 
-				'mrkcode_2000'=>'MTM2MjIyMDAwMDU4ODEwOTE4UVdFUkRGRVdUNTEyMzQ1NllHSEZEU1dFUlQ1NllVSUpIR0ZEU0FFUlRZVUlPS0o4SEdGVkNYWlNETEtKSEdGRFNBT0lQTE1OQkdISllUUkRGR0hKS0lSRVdTREZHSEpJT0lVVERXUUFTREZSRVRZVUlVWUdUUkVERkdIVVlUUkVXUVdF',
 				'imcType_2100'=> 256,
+				'mrkcode_2000'=>'MDEwNDY0MDA3ODAxNjM3MjIxbWc1YTUxO1FIZzJzNx05MUZGRDAdOTJkR1Z6ZENWWlNOK09zTy9FNDcyQTVlb2J4eERlazZXeDNRdTNsN3UyK2c0PQ==',
 				'imcModeProcessing_2102'=>0,
-				'itemEstimatedStatus_2003'=>1
+				'itemEstimatedStatus_2003'=>1,
 				
-			),
-			array(
-				'productName_1030'=>'Отладка программы 2222',
-				'price_1079'=>0,
-				'qty_1023'=>1,
-				"amount_1043"=>0,
-				'unit_2108'=>0,
-				'paymentFormCode_1214'=>4,
-				'productTypeCode_1212'=>1,
-				'tax_1199'=>6,
-
-				'imcType_2100' => 3, 
-				// 01046501047515422155WqK<UW38Cwg91EE1092qIv57HQz9uYwVIWZNq105R3lcHMerZ85Cxw0HWUJuMk=
-				'mrkcode_2000'=>'MDEwNDY1MDEwNDc1MTU0MjIxNTVXcUs8VVczOEN3Zx05MUVFMTAdOTJxSXY1N0hRejl1WXdWSVdaTnExMDVSM2xjSE1lclo4NUN4dzBIV1VKdU1rPQ==',
-				'imcType_2100'=> 256,
-				'imcModeProcessing_2102'=>0,
-				'itemEstimatedStatus_2003'=>1
-
+				'industryInfo_1261'=>[ // надо передавать именно как список
+					array( 
+						'foivId_1262'=>'030', // Министерство промышленности и торговли Российской Федерации
+						'foivDate_1263'=>'21.11.2023',
+						'foidDocNumber_1264'=>'1944',
+						// industryAttribute_1265 заполняем по результату проверки в честном знаке
+						'industryAttribute_1265'=>'UUID=1b48ccb5-33fd-4683-b30c-90a8f3ab36ca&Time=1754567373328'
+					)
+				]
+				
 			)
 		],		
 		'cashierName_1021'=>'Пупкин Иван Трофимович',
@@ -348,11 +339,13 @@ if( $payload != "")
 	curl_close($ch);
 
 	echo "
+		<hr>
 		<h2>Ответ</h2>
 		<pre>$result</pre>";
 
 
 	echo "
+		<hr>
 		<h2>Послали:</h2>
 		<h3>заголовки:</h2>
 		<pre>" . json_encode(  $headers  , JSON_PRETTY_PRINT)."</pre>
